3a620f541f7d7b8dd291bdc1eeaacdc1ea3d046c
ameykore <34702262+ameykore.github.com>
2018-11-01 11:22:18 +0530

    full walk

--- angleMapping/angleMapping.ino/angleMapping.ino.ino
+++ angleMapping/angleMapping.ino/angleMapping.ino.ino
@@ -4,6 +4,7 @@ Servo myservo[8];  // create servo object to control a servo
 
 #define limitlow 1300
 #define limithigh 1500
+#define dly 200
 void stand() {
   //1500 as initial position of all servo
   int  low = 00;
@@ -23,16 +24,15 @@ void setup() {
 void diagonal1_up() {
   myservo[1].writeMicroseconds(1500);
   myservo[3].writeMicroseconds(1500);
-  delay(200);
-  myservo[0].writeMicroseconds(1700);
-  myservo[2].writeMicroseconds(1300);
-  delay(200);
+  delay(dly);
+  myservo[0].writeMicroseconds(1600);
+  myservo[2].writeMicroseconds(1400);
 }
 
 void diagonal1_low() {
   myservo[3].writeMicroseconds(1900);
   myservo[1].writeMicroseconds(1100);
-  delay(200);
+  delay(dly);
   myservo[0].writeMicroseconds(1500);
   myservo[2].writeMicroseconds(1500);
   //delay(200);
@@ -42,34 +42,34 @@ void diagonal1_low() {
 }
 
 void diagonal2_up() {
-  myservo[5].writeMicroseconds(1600);
-  myservo[7].writeMicroseconds(1400);
-  delay(200);
+  myservo[5].writeMicroseconds(1500);
+  myservo[7].writeMicroseconds(1500);
+  delay(dly);
+  myservo[6].writeMicroseconds(1600);
   myservo[4].writeMicroseconds(1400);
-  myservo[6].writeMicroseconds(1300);
-  delay(200);
 }
 
 void diagonal2_low() {
-  myservo[5].writeMicroseconds(limitlow);
-  myservo[7].writeMicroseconds(1600);
-  delay(200);
-  myservo[4].writeMicroseconds(1600);
-  myservo[6].writeMicroseconds(1600);
-  delay(200);
-
+  myservo[5].writeMicroseconds(1900);
+  myservo[7].writeMicroseconds(1100);
+  delay(dly);
+  myservo[4].writeMicroseconds(1500);
+  myservo[6].writeMicroseconds(1500);
+  //delay(200);
+  myservo[5].writeMicroseconds(1500);
+  myservo[7].writeMicroseconds(1500);
 }
 
 void loop() {
   //To walk
   stand();
-  delay(200);
+  delay(dly);
 //  diagonal1_up();
-//  delay(200);
+//  delay(dly);
 //  diagonal1_low();
-//  delay(200);
-  diagonal2_up();
-  delay(200);
-  diagonal2_low();
-  delay(200);
+//  delay(dly);
+//  diagonal2_up();
+//  delay(dly);
+//  diagonal2_low();
+//  delay(dly);
 }
