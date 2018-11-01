6f351988ac5a92442fa0d4434170868f5921facd
ameykore <34702262+ameykore.github.com>
2018-11-01 11:49:38 +0530

    updated Microseconds to angle

--- angleMapping/angleMapping.ino/angleMapping.ino.ino
+++ angleMapping/angleMapping.ino/angleMapping.ino.ino
@@ -2,15 +2,17 @@
 
 Servo myservo[8];  // create servo object to control a servo
 
-#define limitlow 1300
-#define limithigh 1500
+#define MIN 1000
+#define MAX 2000
+#define MID (((MAX-MIN)/2)+MIN)
+#define angleTo_us(x) map(x,-90,90,MIN,MAX)
 #define dly 200
 void stand() {
   //1500 as initial position of all servo
   int  low = 00;
   int  high = 00;
   for (int i = 0; i < 8 ; i++) {
-    myservo[i].writeMicroseconds(1500);
+    myservo[i].writeMicroseconds(angleTo_us(0));
   }
 }
 
@@ -22,54 +24,54 @@ void setup() {
 }
 
 void diagonal1_up() {
-  myservo[1].writeMicroseconds(1500);
-  myservo[3].writeMicroseconds(1500);
+  myservo[1].writeMicroseconds(angleTo_us(0));
+  myservo[3].writeMicroseconds(angleTo_us(0));
   delay(dly);
-  myservo[0].writeMicroseconds(1600);
-  myservo[2].writeMicroseconds(1400);
+  myservo[0].writeMicroseconds(angleTo_us(22));
+  myservo[2].writeMicroseconds(angleTo_us(-22));
 }
 
 void diagonal1_low() {
-  myservo[3].writeMicroseconds(1900);
-  myservo[1].writeMicroseconds(1100);
+  myservo[3].writeMicroseconds(angleTo_us(67));
+  myservo[1].writeMicroseconds(angleTo_us(-67));
   delay(dly);
-  myservo[0].writeMicroseconds(1500);
-  myservo[2].writeMicroseconds(1500);
+  myservo[0].writeMicroseconds(angleTo_us(0));
+  myservo[2].writeMicroseconds(angleTo_us(0));
   //delay(200);
-  myservo[1].writeMicroseconds(1500);
-  myservo[3].writeMicroseconds(1500);
+  myservo[1].writeMicroseconds(angleTo_us(0));
+  myservo[3].writeMicroseconds(angleTo_us(0));
 
 }
 
 void diagonal2_up() {
-  myservo[5].writeMicroseconds(1500);
-  myservo[7].writeMicroseconds(1500);
+  myservo[5].writeMicroseconds(angleTo_us(0));
+  myservo[7].writeMicroseconds(angleTo_us(0));
   delay(dly);
-  myservo[6].writeMicroseconds(1600);
-  myservo[4].writeMicroseconds(1400);
+  myservo[4].writeMicroseconds(angleTo_us(-22));
+  myservo[6].writeMicroseconds(angleTo_us(22));
 }
 
 void diagonal2_low() {
-  myservo[5].writeMicroseconds(1900);
-  myservo[7].writeMicroseconds(1100);
+  myservo[5].writeMicroseconds(angleTo_us(67));
+  myservo[7].writeMicroseconds(angleTo_us(-67));
   delay(dly);
-  myservo[4].writeMicroseconds(1500);
-  myservo[6].writeMicroseconds(1500);
+  myservo[4].writeMicroseconds(angleTo_us(0));
+  myservo[6].writeMicroseconds(angleTo_us(0));
   //delay(200);
-  myservo[5].writeMicroseconds(1500);
-  myservo[7].writeMicroseconds(1500);
+  myservo[5].writeMicroseconds(angleTo_us(0));
+  myservo[7].writeMicroseconds(angleTo_us(0));
 }
 
 void loop() {
   //To walk
   stand();
-  delay(dly);/
-  diagonal1_up();
   delay(dly);
-  diagonal1_low();
-  delay(dly);
-  diagonal2_up();
-  delay(dly);
-  diagonal2_low();
-  delay(dly);
+      diagonal1_up();
+      delay(dly);
+      diagonal1_low();
+      delay(dly);
+      diagonal2_up();
+      delay(dly);
+      diagonal2_low();
+      delay(dly);
 }
