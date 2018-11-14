be6e3881e68696eda9f33cc8813862157382b43f
ameykore <34702262+ameykore@users.noreply.github.com>
2018-11-14 15:03:10 +0530

    hourse walking

--- genericCode/new_start_to_generic_nov13c/new_start_to_generic_nov13c.ino
+++ genericCode/new_start_to_generic_nov13c/new_start_to_generic_nov13c.ino
@@ -6,7 +6,7 @@ Servo myservo[8];   // create servo object to control a servo
 #define MAX 2000
 #define MID (((MAX-MIN)/2)+MIN)
 #define angleTo_us(x) map(x,-90,90,MIN,MAX)
-#define x_len 50                           //assumes angles for now
+#define x_len 90  //assumes angles for now
 #define y_len 50
 #define dly 200
 #define init 0
@@ -17,16 +17,22 @@ void stand() {
   }
 }
 
-void servopin(int a, int b , int ang) {
-  myservo[a].writeMicroseconds(angleTo_us(ang));
+void servopin(int a, int b , int d, int ang1, int ang2, int an) {
+  myservo[a].writeMicroseconds(angleTo_us(ang1));
+  myservo[d].writeMicroseconds(angleTo_us(an));
   delay(dly);
-  myservo[b].writeMicroseconds(angleTo_us(ang));
+  myservo[b].writeMicroseconds(angleTo_us(ang2));
+  myservo[d].writeMicroseconds(angleTo_us(an/2));
+  delay(dly);
+  myservo[a].writeMicroseconds(angleTo_us(init));
+  myservo[d].writeMicroseconds(angleTo_us(init));
   delay(dly);
 }
 
 void walk(int x, int y) {
-  servopin(1, 0, -x);
-  servopin(1, 0, init);
+  servopin(1, 0, 2, -x, -y,50);
+  servopin(3, 2, 0, x, y,-50);
+  // servopin(1, 0, -30, init);
   //  servopin(2, 0, init);  //sends (servo pin x, servo pin y,  angle)
   //  servopin(4, 6 , y);
   //  servopin(5, 7, x);
@@ -37,15 +43,11 @@ void setup() {
     myservo[i].attach( (i + 2) ); //attach servo  2 to 9
   }
   stand();
-  delay(3000);
+  delay(30000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    walk(x_len, y_len);
-    delay(dly);
-//  myservo[0].writeMicroseconds(angleTo_us(0));
-//  delay(dly);
-//  myservo[0].writeMicroseconds(angleTo_us(-90));
-//  delay(dly);
+  walk(x_len, y_len);
+  delay(dly);
 }
