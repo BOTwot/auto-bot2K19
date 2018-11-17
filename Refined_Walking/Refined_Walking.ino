6a1be14f1f06d8cc7f74c9b63eea048c7bf9aa09
Kushal <kushshah777888@gmail.com>
2018-11-17 17:19:11 +0530

    added test cycle

--- Refined_Walking/Refined_Walking.ino
+++ Refined_Walking/Refined_Walking.ino
@@ -1,5 +1,11 @@
 #include <Servo.h>
 
+#ifdef REFRESH_INTERVAL
+#undef REFRESH_INTERVAL
+#endif
+
+#define REFRESH_INTERVAL 50000L
+
 Servo myservo[8];   // create servo object to control a servo
 
 #define MIN 1000
@@ -7,13 +13,15 @@ Servo myservo[8];   // create servo object to control a servo
 #define MID (((MAX-MIN)/2)+MIN)
 #define angleTo_us(x) map(x,-90,90,MIN,MAX)
 #define writeAngle(e) writeMicroseconds(angleTo_us(e))
-#define x 60  //assumes angles for now
-#define y 40
+#define x 40  //assumes angles for now
+#define y 20
 #define dly 200
-#define initial -10
+#define initial1 10
+#define initial2 0
 #define WalkPin 10
 int flag = 0;
 int corr[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
 void stand() {
   cycle5();
 }
@@ -24,8 +32,8 @@ void setup() {
   }
   //  Serial.begin(9600);
   pinMode(WalkPin, INPUT_PULLUP);
-  stand();
-  delay(3000);
+ // stand();
+  //delay(3000);
 }
 
 void writeser(int a, int b, int ang1, int ang2)
@@ -37,37 +45,44 @@ void writeser(int a, int b, int ang1, int ang2)
 void cycle1()
 {
   writeser(1, 0, -x, -y);
-  writeser(3, 2, x, initial);
-  writeser(5, 4, initial, y / 2);
-  writeser(7, 6, -initial, -initial);
+  writeser(3, 2, x, initial2);
+  writeser(5, 4, initial1, y / 2);
+  writeser(7, 6, -initial1, -initial2);
 }
 void cycle2()
 {
-  writeser(1, 0, -initial, -y);
+  writeser(1, 0, -initial1, -y);
   writeser(3, 2, x, y);
-  writeser(5, 4, initial, initial);
-  writeser(7, 6, -x, -initial);
+  writeser(5, 4, initial1, initial2);
+  writeser(7, 6, -x, -initial2);
 }
 void cycle3()
 {
-  writeser(1, 0, -initial, -y/2);
-  writeser(3, 2, initial, y);
-  writeser(5, 4, x, initial);
+  writeser(1, 0, -initial1, -y/2);
+  writeser(3, 2, initial1, y);
+  writeser(5, 4, x, initial2);
   writeser(7, 6, -x, -y);
 }
 void cycle4()
 {
-  writeser(1, 0, -initial, -initial);
-  writeser(3, 2, initial, y / 2);
+  writeser(1, 0, -initial1, -initial2);
+  writeser(3, 2, initial1, y / 2);
   writeser(5, 4, x, y);
-  writeser(7, 6, -initial, -y);
+  writeser(7, 6, -initial1, -y);
 }
 void cycle5()//stand
 {
-  writeser(1, 0, -x, -initial);
-  writeser(3, 2, initial, initial);
-  writeser(5, 4, initial, y);
-  writeser(7, 6, initial, -y / 2);
+  writeser(1, 0, -x, -initial2);
+  writeser(3, 2, initial1, initial2);
+  writeser(5, 4, initial1, y);
+  writeser(7, 6, -initial1, -y / 2);
+}
+void testcycle()
+{
+   writeser(1, 0, -initial1, -initial2);
+  writeser(3, 2, initial1, initial2);
+  writeser(5, 4, initial1, initial2);
+  writeser(7, 6, -initial1, -initial2);
 }
 void walk() {
   cycle1();
@@ -97,7 +112,8 @@ void loop() {
   //    cycle5();
   //    flag=1;
   //  }
-//  walk();
-      stand();
-      delay(dly);
+ // walk();
+     // stand();
+     testcycle();
+     // delay(dly);
 }
